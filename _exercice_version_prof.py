05849cbccc17a58dca187ef568cc1a3f92181e45
aymanechalh <77995760+aymanechalh@users.noreply.github.com>
2021-01-26 14:01:05 -0500

    update

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -14,10 +14,13 @@ def square(a: float) -> float:
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    x=(a+b+c)/3
+
+    return x
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
+
     return 0.0
 
 
